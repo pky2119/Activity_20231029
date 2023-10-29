cd316a83a5fea12a01d073a5a03e47c54c580739
pky2119 <pky2119@naver.com>
2023-10-29 10:24:16 +0900

    3번 화면에서 문구를 받아서 반영

--- app/src/main/java/kr/ac/wku/activity_20231029/ThirdActivity.kt
+++ app/src/main/java/kr/ac/wku/activity_20231029/ThirdActivity.kt
@@ -11,5 +11,13 @@ class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= DataBindingUtil.setContentView(this,R.layout.activity_third)
+
+//        3번 화면이 열히ㅣ면 => 이전 화면에서 보내준 String을 받아서 txtMessage에 반영
+
+//        이전 화면에서 보내준 "message"라는 이름표가 붙어있는 String 추출
+        val receivedMessage = intent.getStringExtra("message")
+
+        binding.txtMessage.text = receivedMessage
+
     }
 }
