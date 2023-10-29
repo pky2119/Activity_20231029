f3d49a763733aeb6ddb6284a4dfb503fe55f6472
pky2119 <pky2119@naver.com>
2023-10-29 09:37:48 +0900

    데이터 바인딩

--- app/src/main/java/kr/ac/wku/activity_20231029/MainActivity.kt
+++ app/src/main/java/kr/ac/wku/activity_20231029/MainActivity.kt
@@ -1,11 +1,16 @@
 package kr.ac.wku.activity_20231029
 
+import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kr.ac.wku.activity_20231029.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
